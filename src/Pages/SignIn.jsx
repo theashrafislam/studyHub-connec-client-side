534bbb916d86275ea324b9768681c48da8dabaa5
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-05-12 12:13:42 +0600

    footer design is complate

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 import toast, { Toaster } from 'react-hot-toast';
 import logo from '../../public/favicon.svg';
+import { Helmet, HelmetProvider } from "react-helmet-async";
 const SignIn = () => {
-    const {signInUser, signInWithGoogle} = useContext(AuthContext)
+    const { signInUser, signInWithGoogle } = useContext(AuthContext)
 
     const handleLoginForm = (event) => {
         event.preventDefault()
@@ -16,7 +17,7 @@ const SignIn = () => {
         signInUser(email, password)
             .then(result => {
                 console.log(result);
-                if(result) {
+                if (result) {
                     toast.success("Welcome back! You're now signed in and ready to go.");
                 }
             })
@@ -37,68 +38,73 @@ const SignIn = () => {
     }
 
     return (
-        <div className="mt-16">
-            <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-                <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}></div>
+        <HelmetProvider>
+            <div className="mt-16">
+                <Helmet>
+                    <title>Sign In || StudyHub Connect</title>
+                </Helmet>
+                <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+                    <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}></div>
 
 
-                <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
-                    <div className="flex justify-center mx-auto">
-                        <h1 className="uppercase font-bold text-xl">Sign In to StudyHub Connect</h1>
-                    </div>
-
-                    <a onClick={handleGoogleSignIn} className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <div className="px-4 py-2">
-                            <FcGoogle className="text-3xl" />
+                    <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+                        <div className="flex justify-center mx-auto">
+                            <h1 className="uppercase font-bold text-xl">Sign In to StudyHub Connect</h1>
                         </div>
 
-                        <span className="w-5/6 px-4 py-3 font-bold text-center">Sign in with Google</span>
-                    </a>
-
-                    <div className="flex items-center justify-between mt-4">
-                        <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
-
-                        <a className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline">or login
-                            with email</a>
-
-                        <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
-                    </div>
-
-                    <form onSubmit={handleLoginForm}>
-                        <div className="mt-4">
-                            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" >Email Address</label>
-                            <input id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="email" name="email" />
-                        </div>
-
-                        <div className="mt-4">
-                            <div className="flex justify-between">
-                                <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" >Password</label>
-                                <a href="#" className="text-xs text-gray-500 dark:text-gray-300 hover:underline">Forget Password?</a>
+                        <a onClick={handleGoogleSignIn} className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <div className="px-4 py-2">
+                                <FcGoogle className="text-3xl" />
                             </div>
 
-                            <input id="loggingPassword" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" name="password" />
+                            <span className="w-5/6 px-4 py-3 font-bold text-center">Sign in with Google</span>
+                        </a>
+
+                        <div className="flex items-center justify-between mt-4">
+                            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+
+                            <a className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline">or login
+                                with email</a>
+
+                            <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
                         </div>
 
-                        <div className="mt-6">
-                            <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                                Sign In
-                            </button>
+                        <form onSubmit={handleLoginForm}>
+                            <div className="mt-4">
+                                <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" >Email Address</label>
+                                <input id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="email" name="email" />
+                            </div>
+
+                            <div className="mt-4">
+                                <div className="flex justify-between">
+                                    <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" >Password</label>
+                                    <a href="#" className="text-xs text-gray-500 dark:text-gray-300 hover:underline">Forget Password?</a>
+                                </div>
+
+                                <input id="loggingPassword" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" name="password" />
+                            </div>
+
+                            <div className="mt-6">
+                                <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                                    Sign In
+                                </button>
+                            </div>
+                        </form>
+
+
+
+                        <div className="flex items-center justify-between mt-4">
+                            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+
+                            <Link to='/sign-up' className="text-xs text-red-500 font-semibold uppercase  hover:underline">or sign up</Link>
+
+                            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
                         </div>
-                    </form>
-
-
-
-                    <div className="flex items-center justify-between mt-4">
-                        <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-
-                        <Link to='/sign-up' className="text-xs text-red-500 font-semibold uppercase  hover:underline">or sign up</Link>
-
-                        <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
                     </div>
                 </div>
+                <Toaster />
             </div>
-            <Toaster/>
-        </div>
+        </HelmetProvider>
     );
 };
 
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,8 +3,9 @@ import logo from '../../public/favicon.svg';
 import { useContext } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import toast, { Toaster } from 'react-hot-toast';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 const SignUp = () => {
-    const {signUpUser, updateUserProfile} = useContext(AuthContext);
+    const { signUpUser, updateUserProfile } = useContext(AuthContext);
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
@@ -30,10 +31,10 @@ const SignUp = () => {
         signUpUser(email, password)
             .then(result => {
                 console.log(result);
-                if(result) {
+                if (result) {
                     updateUserProfile(name, photoURL)
                         .then(() => {
-                            
+
                         })
                         .catch(error => {
                             console.log(error);
@@ -47,78 +48,83 @@ const SignUp = () => {
             })
     }
     return (
-        <div>
-            <section className="bg-white dark:bg-gray-900">
-                <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
-                    <form className="w-full max-w-md" onSubmit={handleFormSubmit}>
-                        <div className="flex justify-center mx-auto">
-                            <img className="w-[150px]" src={logo} alt=""/>
-                        </div>
-
-                        <div className="flex items-center justify-center mt-6">
-                            <Link to='/sign-up' className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white">
-                                sign up
-                            </Link>
-                        </div>
-
-                        <div className="relative flex items-center mt-8">
-                            <span className="absolute">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                </svg>
-                            </span>
-
-                            <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Full Name" name='name'/>
-                        </div>
-
-                        <div className="relative flex items-center mt-8">
-                            <span className="absolute">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                </svg>
-                            </span>
-
-                            <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Photo URL" name='photoURL'/>
-                        </div>
-
-                        <div className="relative flex items-center mt-6">
-                            <span className="absolute">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                                </svg>
-                            </span>
-
-                            <input type="email" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" name='email'/>
-                        </div>
-
-                        <div className="relative flex items-center mt-4">
-                            <span className="absolute">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                                </svg>
-                            </span>
-
-                            <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password" name='password'/>
-                        </div>
-
-                        
-
-                        <div className="mt-6">
-                            <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                                Sign Up
-                            </button>
-
-                            <div className="mt-6 text-center ">
-                                <p className="text-sm text-blue-500 hover:underline dark:text-blue-400">
-                                    Already have an account? <Link className='text-red-500 font-semibold' to='/sign-in'>Sign In</Link>
-                                </p>
+        <HelmetProvider>
+            <div>
+                <Helmet>
+                    <title>Sign Up || StudyHub Connect</title>
+                </Helmet>
+                <section className="bg-white dark:bg-gray-900">
+                    <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
+                        <form className="w-full max-w-md" onSubmit={handleFormSubmit}>
+                            <div className="flex justify-center mx-auto">
+                                <img className="w-[150px]" src={logo} alt="" />
                             </div>
-                        </div>
-                    </form>
-                </div>
-            </section>
-            <Toaster />
-        </div>
+
+                            <div className="flex items-center justify-center mt-6">
+                                <Link to='/sign-up' className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white">
+                                    sign up
+                                </Link>
+                            </div>
+
+                            <div className="relative flex items-center mt-8">
+                                <span className="absolute">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                    </svg>
+                                </span>
+
+                                <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Full Name" name='name' />
+                            </div>
+
+                            <div className="relative flex items-center mt-8">
+                                <span className="absolute">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                    </svg>
+                                </span>
+
+                                <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Photo URL" name='photoURL' />
+                            </div>
+
+                            <div className="relative flex items-center mt-6">
+                                <span className="absolute">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    </svg>
+                                </span>
+
+                                <input type="email" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" name='email' />
+                            </div>
+
+                            <div className="relative flex items-center mt-4">
+                                <span className="absolute">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                                    </svg>
+                                </span>
+
+                                <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password" name='password' />
+                            </div>
+
+
+
+                            <div className="mt-6">
+                                <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                    Sign Up
+                                </button>
+
+                                <div className="mt-6 text-center ">
+                                    <p className="text-sm text-blue-500 hover:underline dark:text-blue-400">
+                                        Already have an account? <Link className='text-red-500 font-semibold' to='/sign-in'>Sign In</Link>
+                                    </p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+                <Toaster />
+            </div>
+        </HelmetProvider>
     );
 };
 
